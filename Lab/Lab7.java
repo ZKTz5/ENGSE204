a8198919b202cb5f4e3350725d0583d658265639
ZKTz5 <krittaphat4521km@gmail.com>
2025-11-22 03:03:56 +0700

    5

---
+++ Lab/Lab7.java
@@ -0,0 +1,21 @@
+package Lab;
+import java.util.Scanner;
+
+public class Lab7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("กรอกจำนวนเ:");
+        int n = scanner.nextInt();
+
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            System.out.print("กรอกจำนวนเต็มที่ " + i + ": ");
+            int number = scanner.nextInt();
+            sum = number + sum;
+        }
+        System.out.println("ผลรวม: " + sum);
+        
+        scanner.close();
+    }    
+}
