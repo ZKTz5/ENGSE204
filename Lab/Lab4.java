8cf9c3e1dd329797814ad06eccca117e7d372429
ZKTz5 <krittaphat4521km@gmail.com>
2025-11-22 01:27:35 +0700

    4

---
+++ Lab/Lab4.java
@@ -0,0 +1,25 @@
+package Lab;
+import java.util.Scanner;
+
+public class Lab4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("กรอกคะแนน:");
+        int score = scanner.nextInt();
+
+        if (score >= 80) {
+            System.out.println("A");
+        } else if (score >= 70) {
+            System.out.println("B");
+        } else if (score >= 60) {
+            System.out.println("C");
+        } else if (score >= 50) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+        scanner.close();
+    }
+    
+}
