176deaecece85a856df6a3dbb1dafc83179bb7a1
ZKTz5 <krittaphat4521km@gmail.com>
2025-11-22 22:26:34 +0700

    1

--- Lab/Lab1.java
+++ Lab/Lab1.java
@@ -4,17 +4,10 @@ import java.util.Scanner;
 public class Lab1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("จำนวนเต็มตัวที่ 1:");
         int num1 = scanner.nextInt();
-
-        System.out.println("จำนวนเต็มตัวที่ 2:");
         int num2 = scanner.nextInt();
-
         int sum = num1 + num2;
-
         System.out.println("ผลลัพธ์ = " + sum);
-        
         scanner.close();
     }
 }
--- Lab/Lab5.java
+++ Lab/Lab5.java
@@ -5,28 +5,18 @@ public class Lab5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("1 = \"Americano\"\r\n" + //
-                        "2 = \"Latte\"\r\n" + //
-                        "3 = \"Espresso\"\r\n" + //
-                        "4 = \"Mocha\"");
-        int coffee = scanner.nextInt();
-        
-        switch (coffee) {
-            case 1:
-                System.out.println("Americano");
-                break;
-            case 2:
-                System.out.println("Latte");
-                break;
-            case 3:
-                System.out.println("Espresso");
-                break;
-            case 4:
-                System.out.println("Mocha");
-                break;
-            default:
-                System.out.println("Invalid menu");
-                break;
+        String coffee;
+        int menu = scanner.nextInt();
+        if (menu == 1) {
+            coffee = "Americano";
+        } else if (menu == 2) {
+            coffee = "Latte";
+        } else if (menu == 3) {
+            coffee = "Espresso";
+        } else if (menu == 4) {
+            coffee = "Mocha";
+        } else {
+            coffee = "Invalid Menu";
         }
         
         scanner.close();
