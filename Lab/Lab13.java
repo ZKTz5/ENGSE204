b24e04bc1fe21e10b066a5383937acce473e84bd
ZKTz5 <krittaphat4521km@gmail.com>
2025-11-22 22:49:45 +0700

    1

---
+++ Lab/Lab13.java
@@ -0,0 +1,34 @@
+package Lab;
+import java.util.Scanner;
+
+public class Lab13 {
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+
+        int r = kb.nextInt();
+        int c = kb.nextInt();
+        int[][] data = new int[r][c];
+
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                System.out.print("Data at [" + i + "][" + j + "]: ");
+                data[i][j] = kb.nextInt();
+            }
+        }
+
+        int ans = 0; 
+        
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                if(data[i][j] == 1) {
+                    ans = ans + 1;
+                }
+            }
+        }
+
+        System.out.println("Online Server = " + ans);
+        
+        kb.close();
+    }
+}
