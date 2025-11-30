57d19d226d758b7ad38896e78e48fe72f0e712b0
ZKTz5 <krittaphat4521km@gmail.com>
2025-11-30 23:30:37 +0700

    1

---
+++ Lab2/Lab2_3/Student.java
@@ -0,0 +1,40 @@
+package Lab2.Lab2_3;
+import java.util.Scanner;
+
+public class Student {
+    private String studentID;
+    private String name;
+
+    public static int studentCount = 0;
+    public Student(String studentID, String name) {
+        this.studentID = studentID;
+        this.name = name;
+        studentCount++;
+    }
+    public String getStudentID() {
+        return this.studentID; 
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int n = keyboard.nextInt();
+        keyboard.nextLine();
+        
+        for (int i = 0; i < n; i++) {
+        String studentID = keyboard.nextLine();
+        String name = keyboard.nextLine();
+        new Student(studentID, name);
+        }
+
+        keyboard.close();
+
+        System.out.println(Student.studentCount);
+    }
+}
