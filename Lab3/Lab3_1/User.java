64d2f305a960fc20dfc2377dfb77e9b89c3c429a
ZKTz5 <krittaphat4521km@gmail.com>
2025-12-06 17:45:53 +0700

    1

--- Lab3/Lab3_1/User.java
+++ Lab3/Lab3_1/User.java
@@ -5,23 +5,30 @@ public class User {
     private String username;
 
     public User(String username) {
+        if (username == null || username.isEmpty()) {
+            throw new IllegalArgumentException("Error : Username cannot be null or empty");
+        } 
+        else if (username.length() < 5) {
+            throw new IllegalArgumentException("Error : Username cannot be null or empty");
+        } 
+        else {
         this.username = username;
+        }
     }
 
-    public String getUsername() {
-        return this.username;
-    }
-
+    public String getUsername() {return this.username;}
 }
 
 class Main {
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-
-        String username = keyboard.nextLine();
-        keyboard.close();
+        try (Scanner keyboard = new Scanner(System.in)) {
+            String username = keyboard.nextLine();
          
-        User u = new User(username);
-        System.out.println(u.getUsername());
-    }
-}        
+            User u = new User(username);
+            
+            System.out.println(u.getUsername());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }     
+}   
