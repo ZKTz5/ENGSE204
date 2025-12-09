951991a5e0c6ef0ab6adf64fd2ae95b7709c8b2e
ZKTz5 <krittaphat4521km@gmail.com>
2025-12-09 20:58:58 +0700

    1

---
+++ Lab3/Lab3_5/DatabaseConnection.java
@@ -0,0 +1,57 @@
+package Lab3.Lab3_5;
+
+import java.util.Scanner;
+
+public class DatabaseConnection {
+    private String connectionString;
+    private boolean connected;
+
+    public DatabaseConnection(String connectionString) {
+        if (connectionString == null || connectionString.isEmpty()) {
+            throw new IllegalArgumentException("Error : Connection string cannot be null or empty");
+        } else {
+            this.connectionString = connectionString;
+            this.connected = false;
+        }
+    }
+
+    public boolean isConnected() {
+        return this.connected;
+    }
+
+    public void connect() {
+        if (!this.connected) {
+            this.connected = true;
+            System.out.println("Connected to " + connectionString);
+        } else {
+            System.out.println("Already connected.");
+        }
+    }
+
+    public void disconnect() {
+        if (this.connected) {
+            this.connected = false;
+            System.out.println("Disconnected");
+        } else {
+            System.out.println("Already disconnected");
+        
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        try (Scanner keyboard = new Scanner(System.in)) {
+            String connectionString = keyboard.nextLine();
+            
+            DatabaseConnection db = new DatabaseConnection(connectionString);
+            db.connect();
+            db.disconnect();
+            db.disconnect();
+            System.out.println(db.isConnected());
+            
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
